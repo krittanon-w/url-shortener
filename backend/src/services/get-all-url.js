3253ba10e7323dc94cfa3a30a68fe4976b0062e0
Krittanon Wisedchart <Krittanon.w@gmail.com>
2019-01-18 03:36:48 +0700

    add git all urls

--- backend/src/services/get-all-url.js
+++ backend/src/services/get-all-url.js
@@ -1,8 +1,25 @@
 const Router = require('express').Router
+const BaseModel = require('./../share/base-model.js')
 
 // init
 const router = new Router()
 
 module.exports = router.get('/urls', (req, res, next) => {
-    res.send('list all of url in db')
+    // get all documents
+    let query = { }
+
+    // find all doc
+    BaseModel.findMany(query, (error, result) => {
+        if (error) {
+            return res.status(500).json({
+                success: false,
+                message: error.message
+            })
+        }
+        return res.json({
+            success: true,
+            data: result
+        })
+    })
+
 })
